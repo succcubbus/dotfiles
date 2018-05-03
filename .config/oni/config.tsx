e0c89cea9d41a5aea660a9c6e4832cd2a0ea29a5
succcubbus <jannis.kaiser2@gmail.com>
2018-05-03 20:55:27 +0200

    oni: add oni config

---
+++ .config/oni/config.tsx
@@ -0,0 +1,38 @@
+import * as React from "/opt/oni/resources/app/node_modules/react"
+import * as Oni from "/opt/oni/resources/app/node_modules/oni-api"
+
+export const activate = (oni: Oni.Plugin.Api) => {
+  console.log("config activated")
+
+  // Input
+  //
+  // Add input bindings here:
+  //
+  oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+  //
+  // Or remove the default bindings here by uncommenting the below line:
+  //
+  // oni.input.unbind("<c-p>")
+}
+
+export const deactivate = (oni: Oni.Plugin.Api) => {
+  console.log("config deactivated")
+}
+
+export const configuration = {
+  "ui.colorscheme": "solarized8_dark",
+
+  "oni.useDefaultConfig": true,
+  "oni.loadInitVim": true,
+  "oni.hideMenu": true,
+
+  "sidebar.enabled": false,
+
+  "editor.fontSize": "14px",
+  "editor.fontFamily": "Fira Code",
+
+  // UI customizations
+  "ui.animations.enabled": true,
+  "ui.fontSmoothing": "auto",
+}

9693ebc11d83ec880e2078e121289f44bf0eb1e6
succcubbus <jannis.kaiser2@gmail.com>
2018-05-10 16:30:55 +0200

    oni: markdown preview, webgl renderer and formatting

--- .config/oni/config.js
+++ .config/oni/config.js
@@ -1,30 +1,29 @@
 "use strict";
 exports.__esModule = true;
 exports.activate = function (oni) {
-    console.log("config activated");
+    console.log('config activated');
     // Input
     //
     // Add input bindings here:
     //
-    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    oni.input.bind('<c-enter>', function () { return console.log('Control+Enter was pressed'); });
     //
     // Or remove the default bindings here by uncommenting the below line:
     //
     // oni.input.unbind("<c-p>")
 };
 exports.deactivate = function (oni) {
-    console.log("config deactivated");
+    console.log('config deactivated');
 };
 exports.configuration = {
-    "ui.colorscheme": "solarized8_dark",
-    "oni.useDefaultConfig": true,
-    "oni.loadInitVim": true,
-    "oni.hideMenu": true,
-    "tabs.mode": "native",
-    "sidebar.enabled": false,
-    "editor.fontSize": "14px",
-    "editor.fontFamily": "Fira Code",
-    // UI customizations
-    "ui.animations.enabled": true,
-    "ui.fontSmoothing": "auto"
+    'ui.colorscheme': 'solarized8_dark',
+    'oni.useDefaultConfig': true,
+    'oni.loadInitVim': true,
+    'oni.hideMenu': true,
+    'tabs.mode': 'native',
+    'sidebar.enabled': false,
+    'experimental.mardownPreview.enabled': true,
+    'editor.fontSize': '14px',
+    'editor.fontFamily': 'Fira Code',
+    'editor.renderer': 'webgl'
 };
--- .config/oni/config.tsx
+++ .config/oni/config.tsx
@@ -1,39 +1,37 @@
-import * as React from "/opt/oni/resources/app/node_modules/react"
-import * as Oni from "/opt/oni/resources/app/node_modules/oni-api"
+import * as React from '/opt/oni/resources/app/node_modules/react';
+import * as Oni from '/opt/oni/resources/app/node_modules/oni-api';
 
 export const activate = (oni: Oni.Plugin.Api) => {
-  console.log("config activated")
+  console.log('config activated');
 
   // Input
   //
   // Add input bindings here:
   //
-  oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+  oni.input.bind('<c-enter>', () => console.log('Control+Enter was pressed'));
 
   //
   // Or remove the default bindings here by uncommenting the below line:
   //
   // oni.input.unbind("<c-p>")
-}
+};
 
 export const deactivate = (oni: Oni.Plugin.Api) => {
-  console.log("config deactivated")
-}
+  console.log('config deactivated');
+};
 
 export const configuration = {
-  "ui.colorscheme": "solarized8_dark",
+  'ui.colorscheme': 'solarized8_dark',
 
-  "oni.useDefaultConfig": true,
-  "oni.loadInitVim": true,
-  "oni.hideMenu": true,
-  "tabs.mode": "native",
+  'oni.useDefaultConfig': true,
+  'oni.loadInitVim': true,
+  'oni.hideMenu': true,
+  'tabs.mode': 'native',
 
-  "sidebar.enabled": false,
+  'sidebar.enabled': false,
+  'experimental.mardownPreview.enabled': true,
 
-  "editor.fontSize": "14px",
-  "editor.fontFamily": "Fira Code",
-
-  // UI customizations
-  "ui.animations.enabled": true,
-  "ui.fontSmoothing": "auto",
-}
+  'editor.fontSize': '14px',
+  'editor.fontFamily': 'Fira Code',
+  'editor.renderer': 'webgl',
+};

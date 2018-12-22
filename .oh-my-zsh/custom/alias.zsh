52881c4b041e1cd5896bf7227df5ae3fde6e7bed
succcubbus <jannis.kaiser2@gmail.com>
2018-12-22 01:04:53 +0100

    zsh: sort aliases, nocorrect cargo

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -1,48 +1,62 @@
+# sane defaults
 alias more='less'
 export LESS='-RmNgiQ'
 
-alias md='mkdir -p'
-alias rd='rmdir'
-
-alias glg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-
 alias shred='shred -f -n3 -z -u'
-
-alias -g G='| grep'
-alias -g W='| wc'
 
 alias gw='gw --console=rich'
 alias gradle='gradle --console=rich'
 
-alias vim='nvim'
-
 alias unp='unp -U'
-
-open() {
-    xdg-open "$@" &
-}
-
-alias ipfs='nocorrect ipfs'
-alias trizen='nocorrect trizen'
-alias yay='nocorrect yay'
 
 alias ffmpeg='ffmpeg -hide_banner'
 alias ffplay='ffplay -hide_banner'
 
-alias youtube-audio='youtube-dl -x -f bestaudio/best --audio-quality 0 --default-search "ytsearch:" --add-metadata --metadata-from-title "(?P<artist>.+?) - (?P<title>.+?)( \(.*\).*)?$" -o "%(title)s.%(ext)s"'
+alias du='du -h'
+alias df='df -h'
+
+alias vim='nvim'
+
+# fs helpers
+
+alias md='mkdir -p'
+alias rd='rmdir'
+
+td() {
+	mkdir -p $1
+	cd $1
+}
+
+# shorter pipes
+
+alias -g G='| grep'
+alias -g W='| wc'
+alias -g L='| less'
+
+# no I did not mean .config
+
+alias trizen='nocorrect trizen'
+alias yay='nocorrect yay'
+alias git='nocorrect git'
+alias cargo='nocorrect cargo'
 
 # psutil python package is throwing errors that mess up the display
 alias glances='glances 2> /dev/null'
 
-alias battery="upower -i $(upower -e | grep BAT) | grep percent | awk '{ print \$2 }'"
+# otherwise too long
+
+alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias glg="glog"
+
+alias youtube-audio='youtube-dl -x -f bestaudio/best --audio-quality 0 --default-search "ytsearch:" --add-metadata --metadata-from-title "(?P<artist>.+?) - (?P<title>.+?)( \(.*\).*)?$" -o "%(title)s.%(ext)s"'
 
 play() {
     mplayer -volume 50 -softvol -novideo \
         <(youtube-dl -f bestaudio/best --audio-quality 0 --default-search "ytsearch:" -o - "$*")
 }
 
-td() {
-	mkdir -p $1
-	cd $1
+alias battery="upower -i $(upower -e | grep BAT) | grep percent | awk '{ print \$2 }'"
+
+open() {
+    xdg-open "$@" &
 }

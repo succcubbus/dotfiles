7fe56a230d44c14a28c54f5c3b427d6cfc4b74d4
succcubbus <jannis.kaiser2@gmail.com>
2019-01-22 21:01:46 +0100

    install: fancy output

--- install.sh
+++ install.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Installing config for existing software"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -sf "$DIR/.vimrc" "$HOME/.vimrc"
-ln -sf "$DIR/.zshrc" "$HOME/.zshrc"
-ln -sf "$DIR/.Xmodmap" "$HOME/.Xmodmap"
-ln -sf "$DIR/.tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$DIR/.gitconfig" "$HOME/.gitconfig"
+install() {
+    echo -e "\e[90minstalling $1"
+    ln -sf "$DIR/$1" "$HOME/$1"
+}
 
-installdir() {
-    if [[ -d "$HOME/$1" ]]; then
-        rm -rf "$HOME/$1"
-        ln -sf "$DIR/$1" "$HOME/$1"
+skip() {
+    echo -e "\e[33mskipping $1"
+}
+
+install_existing() {
+    if [[ -f "$HOME/$1" ]]; then
+        install "$1"
+    else
+        skip "$1"
     fi
 }
 
-installdir ".config/termite"
-installdir ".config/kitty"
-installdir ".config/trizen"
-installdir ".config/yay"
-installdir ".config/nvim"
-installdir ".config/dunst"
-installdir ".config/sway"
+install_dir() {
+    if [[ -d "$HOME/$1" ]]; then
+        rm -rf "$HOME/$1"
+        install "$1"
+    else
+        skip "$1"
+    fi
+}
 
-if [[ -d "$HOME/.config/oni" ]]; then
-    ln -sf "$DIR/.config/oni/config.js" "$HOME/.config/oni/config.js"
-    ln -sf "$DIR/.config/oni/config.tsx" "$HOME/.config/oni/config.tsx"
-fi
+install_in_dir() {
+    if [[ -d "$HOME/$1" ]]; then
+        dir="$1"
+        shift
+        for file in "$@"; do
+            install "$dir/$file"
+        done
+    else
+        skip "$1"
+    fi
+}
 
-if [[ -d "$HOME/.oh-my-zsh/custom" ]]; then
-    ln -sf "$DIR/.oh-my-zsh/custom/alias.zsh" "$HOME/.oh-my-zsh/custom/alias.zsh"
-    ln -sf "$DIR/.oh-my-zsh/custom/startx.zsh" "$HOME/.oh-my-zsh/custom/startx.zsh"
-    ln -sf "$DIR/.oh-my-zsh/custom/kitty.zsh" "$HOME/.oh-my-zsh/custom/kitty.zsh"
-fi
+install ".vimrc"
+install ".zshrc"
+install ".Xmodmap"
+install ".tmux.conf"
+install ".gitconfig"
+install_existing ".gnome2/accels/nemo"
 
-if [[ -d "$HOME/.gnome2/accels" ]]; then
-    ln -sf "$DIR/.gnome2/accels/nemo" "$HOME/.gnome2/accels/nemo"
-fi
+install_dir ".config/termite"
+install_dir ".config/kitty"
+install_dir ".config/trizen"
+install_dir ".config/yay"
+install_dir ".config/nvim"
+install_dir ".config/dunst"
+install_dir ".config/sway"
+
+install_in_dir ".config/oni" "config.js" "config.tsx"
+install_in_dir ".oh-my-zsh/custom" "alias.zsh" "startx.zsh" "kitty.zsh"
+
+echo -e "\e[32mdone"

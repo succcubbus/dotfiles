82215298fa4ba7d1dd20645e9e8e6f10ec1e89d4
succcubbus <jannis.kaiser2@gmail.com>
2019-07-08 22:37:11 +0200

    zsh: add markdown render alias

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -53,6 +53,8 @@ alias cuts='nocorrect cuts'
 
 # otherwise too long
 
+markdown() { consolemd $* | less }
+
 alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias glg="glog"
 

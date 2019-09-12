6da91e5abf525c646fff0d123092823a11db95a6
succcubbus <16743652+succcubbus@users.noreply.github.com>
2019-09-12 21:43:38 +0200

    sway: improve battery script

--- .config/sway/battery-warn.sh
+++ .config/sway/battery-warn.sh
@@ -2,23 +2,23 @@
 
 ALERTED=false
 
-while [ true ]; do
-    STATUS=`upower -i $(upower -e | grep BAT)`
+while true; do
+  STATUS=$(upower -i "$(upower -e | grep BAT)")
 
-    if [[ "$ALERTED" = false ]]; then
-        if [[ -n `echo "$STATUS" | grep discharging` ]]; then
-            PERCENT=`echo "$STATUS" | grep percent | grep -Eo '[0-9]+'`
-            if [[ "$PERCENT" -le 8 ]]; then
-                REMAINING=`echo "$STATUS" | grep 'time to empty' | awk '{ print $4,$5 }'`
-                notify-send -a battery -u critical "battery low" "~$REMAINING remaining";
-                ALERTED=true
-            fi
-        fi
+  if [[ "$ALERTED" == false ]]; then
+    if echo "$STATUS" | grep -q 'discharging'; then
+      PERCENT=$(echo "$STATUS" | grep 'percent '| grep -Eo '[0-9]+')
+      if [[ "$PERCENT" -le 8 ]]; then
+        REMAINING=$(echo "$STATUS" | grep 'time to empty' | awk '{ print $4,$5 }')
+        notify-send -a battery -u critical "battery low" "~$REMAINING remaining"
+        ALERTED=true
+      fi
     fi
-
-    if [[ -n `echo "$STATUS" | grep ' charging'` ]]; then
-        ALERTED=false
+  else
+    if echo "$STATUS" | grep -q ' charging'; then
+      ALERTED=false
     fi
+  fi
 
-    sleep 1m
+  sleep 1m
 done

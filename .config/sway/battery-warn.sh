18802a027c95d5693c247bc55e61912f9f026c4d
succcubbus <16743652+succcubbus@users.noreply.github.com>
2019-11-14 23:41:50 +0100

    sway: more robust battery state checking

--- .config/sway/battery-warn.sh
+++ .config/sway/battery-warn.sh
@@ -6,7 +6,7 @@ while true; do
   STATUS=$(upower -i "$(upower -e | grep BAT)")
 
   if [[ "$ALERTED" == false ]]; then
-    if echo "$STATUS" | grep -q 'discharging'; then
+    if echo "$STATUS" | grep -q 'state:\s*discharging'; then
       PERCENT=$(echo "$STATUS" | grep 'percentage: '| grep -Eo '[0-9]+')
       REMAINING=$(echo "$STATUS" | grep 'time to empty' | awk '{ print $4,$5 }')
       REMAINING_MIN=$(units -t -- "$REMAINING" "minutes")
@@ -19,7 +19,7 @@ while true; do
       fi
     fi
   else
-    if echo "$STATUS" | grep -q ' charging'; then
+    if echo "$STATUS" | grep -q 'state:\s*charging'; then
       ALERTED=false
     fi
   fi

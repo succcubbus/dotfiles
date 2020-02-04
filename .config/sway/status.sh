a0ae3057bd06365f8593b93e9566aa0b67e1006f
iyzana <16743652+iyzana@users.noreply.github.com>
2020-02-04 22:04:49 +0100

    sway: moar beautiful status

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,13 +1,12 @@
 while true; do
     battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
-    battery_pct=$(echo "$battery" | grep percent | awk '{ print $2 }')
+    battery_pct=$(echo "$battery" | grep percent | awk '{ print $2 }' | tr -d '%')
+    charging=""
     if echo "$battery" | grep 'state:\s*charging' > /dev/null; then
-        charging="+"
-    else
-        charging=""
+        charging="🗲"
     fi
     date=$(date +'%a, %Y-%m-%d %H:%M:%S')
 
-    echo "$battery_pct$charging - $date"
+    echo "$date   $battery_pct％$charging"
     sleep 1
 done

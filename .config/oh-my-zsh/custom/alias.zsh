e9176ebbb753e50ef81edc4dea01febd1da6f1ee
iyzana <16743652+iyzana@users.noreply.github.com>
2021-02-13 01:25:26 +0100

    zsh: add git switch fuzzy alias

--- .config/oh-my-zsh/custom/alias.zsh
+++ .config/oh-my-zsh/custom/alias.zsh
@@ -59,6 +59,7 @@ markdown() { consolemd $* | less }
 
 alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias glg="glog"
+alias gswf='git switch $(git branch -r | sed "s#  origin/##" | grep -v HEAD | fzf)'
 
 alias youtube-audio='youtube-dl -x -f bestaudio/best --audio-quality 0 --default-search "ytsearch:" --add-metadata --metadata-from-title "(?P<artist>.+?) - (?P<title>.+?)( \(.*\).*)?$" -o "%(title)s.%(ext)s"'
 
